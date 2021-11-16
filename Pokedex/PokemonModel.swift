ecb7feb0b523097578572f88a105d264bc5ec417
Denis Truong <denistruong1@gmail.com>
2021-11-16 17:06:52 -0600

    -added properties (attack, defense, height, weight) -PokemonModel renamed to PokemonViewModel and moved to a separate file - Pokemon ViewModel conforms to ObservableObject - @Published pokemon array that gets initialized in the viewmodel  using new   try await.

--- Pokedex/PokemonModel.swift
+++ Pokedex/PokemonModel.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Pokemon: Identifiable, Decodable {
     let pokemonID = UUID()
+    var isFavorite = false
     
     let id: Int
     let name: String
@@ -16,13 +17,22 @@ struct Pokemon: Identifiable, Decodable {
     let type: String
     let description: String
     
+    let attack: Int
+    let defense: Int
+    let height: Int
+    let weight: Int
+    
+    
     enum CodingKeys: String, CodingKey {
         case id
         case name
         case imageURL = "imageUrl"
         case type
         case description
-        
+        case attack
+        case defense
+        case height
+        case weight
     }
     
     
@@ -60,23 +70,6 @@ enum FetchError: Error {
     case badData
 }
 
-class PokemonModel {
-    func getPokemon() async throws -> [Pokemon] {
-        guard let url = URL(string: "https://pokedex-bb36f.firebaseio.com/pokemon.json")
-        else {throw FetchError.badURL}
-        
-        
-        let urlRequest = URLRequest(url: url)
-        let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {throw FetchError.badResponse}
-        guard let data = data.removeNullsFrom(string: "null,") else {throw FetchError.badData}
-        
-        let maybePokemonData = try JSONDecoder().decode([Pokemon].self, from: data)
-        return maybePokemonData
-    }
-    
-}
 
 
 extension Data {
--- Pokedex/PokemonViewModel.swift
+++ Pokedex/PokemonViewModel.swift
@@ -6,6 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
 
 
-
+class PokemonViewModel: ObservableObject {
+    
+    @Published var pokemon = [Pokemon]()
+    
+    init(){
+        Task {
+            pokemon = try await getPokemon()
+        }
+    }
+    
+    func getPokemon() async throws -> [Pokemon] {
+        guard let url = URL(string: "https://pokedex-bb36f.firebaseio.com/pokemon.json")
+        else {throw FetchError.badURL}
+        
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {throw FetchError.badResponse}
+        guard let data = data.removeNullsFrom(string: "null,") else {throw FetchError.badData}
+        
+        let maybePokemonData = try JSONDecoder().decode([Pokemon].self, from: data)
+        return maybePokemonData
+    }
+    
+    
+    let MOCK_POKEMON = Pokemon(id: 0, name: "Kakuna", imageURL: "https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FF4B3A60B-7DA9-4572-AE0B-EA929F80CE5A?alt=media&token=9e61759c-4ba6-4d73-9784-6fad5a973618", type: "Flying", description: "Protective of sprawling territorial area", attack: 60, defense: 55, height: 6, weight: 100)
+    
+}
