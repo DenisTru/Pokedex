4431cd3f17eeef9b4b17335c4936a2f391537831
Denis Truong <denistruong1@gmail.com>
2021-11-18 00:54:44 -0600

    adding decodable conformance

--- Pokedex/Pokemon+CoreDataClass.swift
+++ Pokedex/Pokemon+CoreDataClass.swift
@@ -13,5 +13,45 @@ import CoreData
 
 @objc(CDPokemon)
 public class CDPokemon: NSManagedObject {
-   
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case pokemonID
+        case isFavorite
+        case name
+        case imageURL = "imageUrl"
+        case type
+        case description
+        case attack
+        case defense
+        case height
+        case weight
+    }
+    
+    enum DecoderConfigurationError: Error {
+        case missingManagedObjectContext
+    }
+    
+    required public convenience init(from decoder: Decoder) throws {
+        guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext else {
+            throw DecoderConfigurationError.missingManagedObjectContext
+        }
+        self.init(context: context)
+        
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int32.self, forKey: .id)
+        self.pokemonID = try container.decode(UUID.self, forKey: .pokemonID)
+        self.isFavorite = try container.decode(Bool.self, forKey: .isFavorite)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.imageURL = try container.decode(String.self, forKey: .imageURL)
+        self.type = try container.decode(String.self, forKey: .type)
+        self.descript = try container.decode(String.self, forKey: .description)
+        self.attack = try container.decode(Int32.self, forKey: .attack)
+        self.defense = try container.decode(Int32.self, forKey: .defense)
+        self.height = try container.decode(Int32.self, forKey: .height)
+        self.weight = try container.decode(Int32.self, forKey: .weight)
+        
+        
+    }
 }
